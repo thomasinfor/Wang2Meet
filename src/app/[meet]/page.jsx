3c84a9b6eb26adb6278770c286a4c8d7c4020093
thomasinfor <thomaswang2003@gmail.com>
2025-02-18 09:57:47 -0600

    feature: show view page on entry for previously filled events

--- src/app/[meet]/page.jsx
+++ src/app/[meet]/page.jsx
@@ -2,12 +2,19 @@
 import React from "react";
 import { useEffect } from "react";
 import { useRouter, usePathname } from 'next/navigation';
+import { useAuth } from "@/context/Auth";
+import { useConfig } from "./MeetPanel";
 
 export default function MeetDefault() {
   const router = useRouter();
   const pathname = usePathname();
+  const { config } = useConfig();
+  const { user } = useAuth();
   useEffect(() => {
-    router.replace(`${pathname}/edit`);
+    if (config?.collection[user?.email])
+      router.replace(`${pathname}/view`);
+    else
+      router.replace(`${pathname}/edit`);
   }, [pathname, router]);
   return <></>;
 }
